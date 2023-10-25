6748310037ec15146238444b11d79578c95b1542
Leo Booth <leoboothtx@gmail.com>
2023-10-25 11:36:25 -0500

    completed first test; verify job search table row leads to matching job description

--- test/specs/fidelity-tests.ts
+++ test/specs/fidelity-tests.ts
@@ -1,45 +1,119 @@
 import { browser } from '@wdio/globals';
 import { expect } from '@wdio/globals';
-// import DAHome from '../pageobjects/fidelity-da.page';
 
 describe('Fidelity tests', () => {
 
-    // it('should open Fidelity Digital Assets website', async () => {
-    //     await DAHome.navigateToPage;
-    //     await expect(browser).toHaveUrl(DAHome.getUrl);
-    //     await expect(DAHome.logo).toBeExisting();
-    // })
+    // let assert = require('assert');
+
+    before("Execute before all tests", function(){
+        browser.deleteCookies();
+    });
 
     it('look for a job description on first page', async () => {
         let url : string = "https://www.fidelityinvestments.ie/";
         await browser.url(url);
         await expect(browser).toHaveUrl(url);
 
-        let logoSelector: string = "//button[@id='onetrust-accept-btn-handler']";
-        (await $(logoSelector)).click;
 
-        await expect(browser).toHaveUrl("https://www.fidelityinvestments.ie/");
+        let acceptAllCookies: string = "//button[@id='onetrust-accept-btn-handler']";
+        await $(acceptAllCookies).waitForDisplayed({timeout: 5000});
+        await $(acceptAllCookies).click();
 
-        let searchJobsButton: string = "//ul[@id='menu-main-menu']/descendant::a[@href='https://www.fidelityinvestments.ie/job-search-results/']";
-        (await $(searchJobsButton)).click;
-    
+        let searchJobsButtonXpath: string = "//ul[@id='menu-main-menu']/descendant::a[@href='https://www.fidelityinvestments.ie/job-search-results/']";
+        const searchJobsButton = await $(searchJobsButtonXpath);
+        await searchJobsButton.click();
+
         await expect(browser).toHaveUrl("https://www.fidelityinvestments.ie/job-search-results/");
-
-        // const jobTableRows : any = await $$("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a");
-        let jobTitle = "";
+        
         let expectedJobTitle : string = "Senior QA Engineer";
 
+        await $("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a").waitForExist();
+        await $("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a[1]").scrollIntoView();
+
         for await (let jobTableRow of $$("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a")) {
-            let rowText = await jobTableRow.getText();
+            let rowText : string = await jobTableRow.getText();
             console.log(rowText);
 
             if (rowText === expectedJobTitle) {
-                jobTitle = rowText;
+                // assert.strictEqual(rowText, expectedJobTitle);
+                jobTableRow.click();
                 break;
             }
         }
 
-        await expect(jobTitle).toHaveTextContaining(expectedJobTitle);
+        await expect(browser).toHaveUrl("https://www.fidelityinvestments.ie/job-search-results/");
 
+        await $("//div[contains(@class,'tab-content')]").scrollIntoView();
+        const jobDescriptionText = $("//div[contains(@class,'tab-content')]/descendant::div[contains(@class,'active')]/descendant::div[@data-field='JobDescription']/descendant::p[contains(text(),'As a Senior Quality Engineer')]");
+        await expect(jobDescriptionText).toHaveTextContaining("Senior Quality Engineer");
     });
+
+    // it('look for a job description on all pages', async () => {
+    //     let url : string = "https://www.fidelityinvestments.ie/";
+    //     await browser.url(url);
+    //     await expect(browser).toHaveUrl(url);
+
+    //     may have to clear cookies after each test
+    //     let acceptAllCookies: string = "//button[@id='onetrust-accept-btn-handler']";
+    //     await $(acceptAllCookies).waitForDisplayed({timeout: 5000});
+    //     await $(acceptAllCookies).click();
+
+    //     await $(acceptAllCookies).waitForDisplayed({timeout: 5000, reverse: true });
+
+    //     let searchJobsButtonXpath: string = "//ul[@id='menu-main-menu']/descendant::a[@href='https://www.fidelityinvestments.ie/job-search-results/']";
+    //     const searchJobsButton = await $(searchJobsButtonXpath);
+    //     await searchJobsButton.click();
+
+    //     await expect(browser).toHaveUrl("https://www.fidelityinvestments.ie/job-search-results/");
+        
+    //     let expectedJobTitle : string = "Senior QA Engineer";
+
+    //     await $("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a").waitForExist();
+    //     await $("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a[1]").scrollIntoView();
+
+    //     for await (let jobTableRow of $$("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a")) {
+    //         let rowText : string = await jobTableRow.getText();
+    //         console.log(rowText);
+
+    //         if (rowText === expectedJobTitle) {
+    //             jobTableRow.click();
+    //             break;
+    //         }
+    //     }
+
+        
+    // });
+
+    // it('look for a job description that should not exist', async () => {
+    //     let url : string = "https://www.fidelityinvestments.ie/";
+    //     await browser.url(url);
+    //     await expect(browser).toHaveUrl(url);
+
+    //     let acceptAllCookies: string = "//button[@id='onetrust-accept-btn-handler']";
+    //     await $(acceptAllCookies).waitForDisplayed({timeout: 5000});
+    //     await $(acceptAllCookies).click();
+
+    //     await $(acceptAllCookies).waitForDisplayed({timeout: 5000, reverse: true });
+
+    //     let searchJobsButtonXpath: string = "//ul[@id='menu-main-menu']/descendant::a[@href='https://www.fidelityinvestments.ie/job-search-results/']";
+    //     const searchJobsButton = await $(searchJobsButtonXpath);
+    //     await searchJobsButton.click();
+
+    //     await expect(browser).toHaveUrl("https://www.fidelityinvestments.ie/job-search-results/");
+        
+    //     let expectedJobTitle : string = "Senior QA Engineer";
+
+    //     await $("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a").waitForExist();
+    //     await $("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a[1]").scrollIntoView();
+
+    //     for await (let jobTableRow of $$("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a")) {
+    //         let rowText : string = await jobTableRow.getText();
+    //         console.log(rowText);
+
+    //         if (rowText === expectedJobTitle) {
+    //             jobTableRow.click();
+    //             break;
+    //         }
+    //     }
+    // });
 });
