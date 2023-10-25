7ca58a6260d8b16b9992e579809f6f1d6e81fea3
Leo Booth <leoboothtx@gmail.com>
2023-10-24 22:18:48 -0500

    added browser URL check to test

--- test/specs/fidelity-tests.ts
+++ test/specs/fidelity-tests.ts
@@ -1,12 +1,14 @@
+import { browser } from '@wdio/globals';
 import { expect } from '@wdio/globals';
 import DAHome from '../pageobjects/fidelity-da.page';
 
 describe('Fidelity tests', () => {
 
     let url = "https://www.fidelitydigitalassets.com/";
-    it('should test something', async () => {
+    it('should open Fidelity Digital Assets website', async () => {
 
         await DAHome.openBrowserAtUrl(url);
+        await expect(browser).toHaveUrl(url);
         await expect(DAHome.logo).toBeExisting();
     })
 });
