17fd3c9196a8e10db47b36b8d1c4ee4a732ec906
Leo Booth <leoboothtx@gmail.com>
2023-10-25 08:00:11 -0500

    job search results test

---
+++ test/pageobjects/fidelityinv-ie.page.ts
@@ -0,0 +1,24 @@
+// import { $ } from '@wdio/globals'
+// import Page from './page';
+
+// class DAHome extends Page {
+
+//     url : string = "https://www.fidelityinvestments.ie";
+
+//     public get navigateToPage() {
+//         return this.openBrowserAtUrl(this.url);
+//     }
+
+//     public get getUrl() {
+//         return this.url;
+//     }
+
+//     async public get acceptCookies() {
+//         const acceptCookiesButton = $("//button[@id='onetrust-accept-btn-handler']");
+//         await acceptCookiesButton.click();
+
+//     }
+
+// }
+
+// export default new DAHome();
--- test/specs/fidelity-tests.ts
+++ test/specs/fidelity-tests.ts
@@ -1,21 +1,45 @@
 import { browser } from '@wdio/globals';
 import { expect } from '@wdio/globals';
-import DAHome from '../pageobjects/fidelity-da.page';
+// import DAHome from '../pageobjects/fidelity-da.page';
 
 describe('Fidelity tests', () => {
 
-    it('should open Fidelity Digital Assets website', async () => {
-        await DAHome.navigateToPage;
-        await expect(browser).toHaveUrl(DAHome.getUrl);
-        await expect(DAHome.logo).toBeExisting();
-    })
+    // it('should open Fidelity Digital Assets website', async () => {
+    //     await DAHome.navigateToPage;
+    //     await expect(browser).toHaveUrl(DAHome.getUrl);
+    //     await expect(DAHome.logo).toBeExisting();
+    // })
 
-    it('should open Fidelity Digital Assets website without using page object', async () => {
-        let url : string = "https://www.fidelitydigitalassets.com/";
+    it('look for a job description on first page', async () => {
+        let url : string = "https://www.fidelityinvestments.ie/";
         await browser.url(url);
         await expect(browser).toHaveUrl(url);
 
-        let logoSelector: string = "//header/descendant::a[descendant::img[contains(@class,'fdas-logo')]]";
-        await expect($(logoSelector)).toBeExisting();
+        let logoSelector: string = "//button[@id='onetrust-accept-btn-handler']";
+        (await $(logoSelector)).click;
+
+        await expect(browser).toHaveUrl("https://www.fidelityinvestments.ie/");
+
+        let searchJobsButton: string = "//ul[@id='menu-main-menu']/descendant::a[@href='https://www.fidelityinvestments.ie/job-search-results/']";
+        (await $(searchJobsButton)).click;
+    
+        await expect(browser).toHaveUrl("https://www.fidelityinvestments.ie/job-search-results/");
+
+        // const jobTableRows : any = await $$("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a");
+        let jobTitle = "";
+        let expectedJobTitle : string = "Senior QA Engineer";
+
+        for await (let jobTableRow of $$("//div[@id='widget-jobsearch-results-list']/descendant::div[@class='jobTitle']/a")) {
+            let rowText = await jobTableRow.getText();
+            console.log(rowText);
+
+            if (rowText === expectedJobTitle) {
+                jobTitle = rowText;
+                break;
+            }
+        }
+
+        await expect(jobTitle).toHaveTextContaining(expectedJobTitle);
+
     });
 });
